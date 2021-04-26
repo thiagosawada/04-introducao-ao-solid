98b2a44d6793a478726bb824f3cfcec7700c53d8
Thiago <tms1_tms2@hotmail.com>
2021-04-26 19:07:17 -0300

    Route tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,20 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User does not exist");
+
+    if (!user.admin) throw new Error("Access denied. User is not admin");
+
+    return this.usersRepository.list();
   }
 }
 
