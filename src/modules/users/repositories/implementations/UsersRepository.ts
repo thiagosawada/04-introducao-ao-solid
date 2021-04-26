8fae70329ba4dc3ec841b59aeef363a44d8a20ab
Thiago <tms1_tms2@hotmail.com>
2021-04-26 17:03:42 -0300

    Find user by email or id and turn them to admin

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -34,15 +34,18 @@ class UsersRepository implements IUsersRepository {
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    Object.assign(receivedUser, { admin: true, updated_at: new Date() });
+    return receivedUser;
   }
 
   list(): User[] {
